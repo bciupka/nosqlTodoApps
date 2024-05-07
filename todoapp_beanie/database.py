1d68edfa9295711fb9e7eab714d803650a960f46
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-05-07 08:15:38 +0200

    fully functional minimalistic version of Beanie Todo

--- todoapp_beanie/database.py
+++ todoapp_beanie/database.py
@@ -1,4 +1,4 @@
-from model import Todo
+from model import Todo, TodoUpdate
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 
@@ -20,3 +20,26 @@ async def todo_list():
 async def todo_create(data: Todo):
     await data.insert()
     return data
+
+
+async def todo_get(title: str):
+    todo = await Todo.find_one(Todo.title == title)
+    return todo
+
+
+async def todo_update(title: str, updated: TodoUpdate):
+    todo = await Todo.find_one(Todo.title == title)
+    if todo:
+        todo.description = updated.description
+        todo.due_date = updated.due_date
+        await todo.save()
+
+    return todo
+
+
+async def todo_delete(title: str):
+    todo = await Todo.find_one(Todo.title == title)
+    if todo:
+        deleted = await todo.delete()
+        return deleted
+    return
--- todoapp_beanie/main.py
+++ todoapp_beanie/main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
 from contextlib import asynccontextmanager
-from database import init, todo_create, todo_list
+from database import init, todo_create, todo_list, todo_get, todo_update, todo_delete
 from model import Todo, TodoUpdate
 
 
@@ -23,3 +24,31 @@ async def add_todo(data: Todo) -> Todo:
 async def list_todos() -> list[Todo]:
     todos = await todo_list()
     return todos
+
+
+@app.get("/get_todo")
+async def get_todo(title: str) -> Todo:
+    todo = await todo_get(title)
+    if todo:
+        return todo
+    raise HTTPException(404, "No such Todo in DB")
+
+
+@app.put("/update_todo")
+async def update_todo(title: str, updated: TodoUpdate) -> Todo:
+    todo = await todo_update(title, updated)
+    if todo:
+        return todo
+    raise HTTPException(
+        status.HTTP_406_NOT_ACCEPTABLE, "Cant update Todo that doesnt exist"
+    )
+
+
+@app.delete("/delete_todo", status_code=204)
+async def delete_todo(title: str):
+    result = await todo_delete(title)
+    if result:
+        return
+    raise HTTPException(
+        status.HTTP_406_NOT_ACCEPTABLE, "Cant delete Todo that doesnt exist"
+    )
